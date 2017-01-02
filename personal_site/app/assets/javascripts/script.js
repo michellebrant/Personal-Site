824d5148633aa4eb126d0aa764cfdb8b8765f69f
Michelle Brant <michellebrant>
2017-01-02 12:18:18 -0500

    fixed bootstrap issue

--- personal_site/app/assets/javascripts/script.js
+++ personal_site/app/assets/javascripts/script.js
@@ -8,7 +8,8 @@ fontArray = [
 "'Nova Script', cursive",
 "'Indie Flower', cursive",
 "'Lobster', cursive",
-"'Bungee', cursive"
+"'Bungee', cursive",
+'"Pacifico", cursive'
 ]
 
 function changeFonts(){
@@ -23,4 +24,29 @@ setInterval(function(){
 }, 1000)
 
 
+function changeMFonts(){
+  var randomNum = Math.floor(Math.random() * 9);
+  font = fontArray[randomNum]
+  $('.name').css("font-family", font)
+}
 
+
+$('.name').mouseover(function(){
+  namei = setInterval(function(){
+  changeMFonts();
+}, 500)
+
+})
+
+$('.name').mouseleave(function(){
+  clearInterval(namei)
+})
+
+$(window).bind('scroll', function() {
+     if ($(window).scrollTop() > 100) {
+         $('.learnmore').hide();
+     }
+     else {
+         $('.learnmore').show();
+     }
+});
