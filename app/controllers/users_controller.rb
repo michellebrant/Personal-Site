81c6f9b33b87df803e69f7993e65c9f8585d2578
Michelle Brant <michelle.brant18@gmail.com>
2017-01-30 13:53:25 -0500

    make it static

--- app/controllers/users_controller.rb
+++
@@ -1,9 +0,0 @@
-class UsersController < ApplicationController
-  def create
-  Users.create(name: params[:name],
-                email: params[:email],
-                message: params[:message] )
-      redirect_to "/users"
-
-end
-end
