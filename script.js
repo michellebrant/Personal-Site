f163aacd75b6f0e0b2187b1fd5853e42268cc3ad
Michelle Brant <michelle.brant18@gmail.com>
2017-02-07 16:00:37 -0500

    hover links

--- script.js
+++ script.js
@@ -24,24 +24,6 @@ $(document).ready(function () {
 //   changeFonts();
 // }, 1000)
 
-
-// function changeMFonts(){
-//   var randomNum = Math.floor(Math.random() * 9);
-//   font = fontArray[randomNum]
-//   $('.name').css("font-family", font)
-// }
-
-$('.name').mouseenter(function(){
-  namei = setInterval(function(){
-  changeMFonts();
-}, 500)
-
-})
-
-$('.name').mouseleave(function(){
-  clearInterval(namei)
-})
-
 $(window).bind('scroll', function() {
      if ($(window).scrollTop() > 100) {
          $('.learnmore').hide();
